64e9bab4ecec2219f5a5a6e291a2cb9c226be5ba
James Nolan <jamesn8391@gmail.com>
2022-12-16 21:57:05 -0600

    added welcome screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 use termion::event::Key;
 use crate::Terminal;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor{ //construtor
     should_quit: bool,
     terminal: Terminal,
@@ -21,16 +23,18 @@ impl Editor{
             }
         }
     }
+
     pub fn default() -> Self{
         Self {
             should_quit:false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
         }
     }
+
     fn refresh_screen(&self) -> Result<(), std::io::Error>{ //clearing screen and drawing rows
         Terminal::cursor_hide(); //cursor hide and show are to hide the cursor when drawing to the screen
-        Terminal::clear_screen();
         if self.should_quit{
+            Terminal::clear_screen();
             println!("Goodbye. \r");
         }
         else{
@@ -49,9 +53,27 @@ impl Editor{
         }
         Ok(())
     }
+
+    fn draw_welcome_message(&self){ //setup to support any terminal size
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2; //center screen
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message); //addes space formatting
+        welcome_message.truncate(width); //shortens if needed
+        println!("{}\r", welcome_message);
+    }
+
     fn draw_rows(&self){ //drawing ~'s at the beginning
-        for _ in 0..self.terminal.size().height -1{
-            println!("~\r");
+        let height = self.terminal.size().height;
+        for row in 0 .. height -1{
+            Terminal::clear_current_line();
+            if row == height / 3{
+                self.draw_welcome_message();
+            }else{
+                println!("~\r")
+            }
         }
     }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -24,20 +24,25 @@ impl Terminal{
             _stdout: stdout().into_raw_mode()?,
         })
     }
+    
     pub fn size(&self) -> &Size{ //accessor
         &self.size
     }
+
     pub fn clear_screen(){ //clears screen
         print!("{}", termion::clear::All);
     }
+
     pub fn cursor_position(x: u16, y: u16){ //reset cursor
         let x = x.saturating_add(1); //do this to hide that termion is 1 based, not 0 based
         let y = y.saturating_add(1);
         print!("{}", termion::cursor::Goto(x,y));
     }
+
     pub fn flush() -> Result<(), std::io::Error>{ //flush
         io::stdout().flush()
     }
+
     pub fn read_key() -> Result<Key, std::io::Error>{ //reading keys from stdin
         loop{
             if let Some(key) = io::stdin().lock().keys().next(){
@@ -45,10 +50,16 @@ impl Terminal{
             }
         }
     }
+
     pub fn cursor_hide(){
         print!("{}", termion::cursor::Hide);
     }
+
     pub fn cursor_show(){
         print!("{}", termion::cursor::Show);
+    }
+
+    pub fn clear_current_line(){
+        print!("{}", termion::clear::CurrentLine);
     }
 }
